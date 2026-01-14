666921c76e066e35d138b1142b5f0f5ba67bb899
Abdisalanheilow <Abdisalanheilow@gmail.com>
2026-01-14 19:38:37 +0300

    realestate

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use App\Models\User;
+
+class AdminController extends Controller
+{
+    public function AdminDashboard()
+    {
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.index', compact('profileData'));
+    }
+
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+    public function AdminLogin()
+    {
+        return view('Admin.admin_login');
+    }
+
+}
